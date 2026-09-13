6e3989fbf0a793eca899d0b301524600f34c18aa
KurobeerSabasand <k.abe.6342@gmail.com>
2026-09-13 16:43:04 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/wms-frontend-vue/',
+  base: '/',   // GitHub Pages の設定を消す
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
